82b0728e4a8052acd862bc2b4d8bb5f8210e4a87
Norman Alié <mail@normanalie.fr>
2024-03-01 10:56:00 +0100

    docstring
    Documentation of PPM library

--- ppm.h
+++ ppm.h
@@ -1,12 +1,28 @@
+/**
+ * @file ppm.h
+ * @brief Library for PPM image file generation.
+ */
+
 #ifndef PPM_H
 #define PPM_H
 
+/**
+ * @struct ppm_pixel
+ * @brief Structure representing a single pixel in a PPM image.
+ */
 struct ppm_pixel {
-  unsigned char r;
-  unsigned char g;
-  unsigned char b;
+  unsigned char r; /**< Red channel value */
+  unsigned char g; /**< Green channel value */
+  unsigned char b; /**< Blue channel value */
 };
 
+/**
+ * @brief Sets the RGB values of a pixel.
+ * @param px Pointer to the pixel to be set.
+ * @param r Red channel value.
+ * @param g Green channel value.
+ * @param b Blue channel value.
+ */
 static inline void ppm_setpixel(struct ppm_pixel *px, unsigned char r,
                                 unsigned char g, unsigned char b) {
   px->r = r;
@@ -14,15 +30,41 @@ static inline void ppm_setpixel(struct ppm_pixel *px, unsigned char r,
   px->b = b;
 }
 
+/**
+ * @struct ppm_image
+ * @brief Structure representing a PPM image.
+ */
 struct ppm_image {
-  unsigned int width;
-  unsigned int height;
-  struct ppm_pixel *px;
+  unsigned int width; /**< Width of the image */
+  unsigned int height; /**< Height of the image */
+  struct ppm_pixel *px; /**< Pixel data */
 };
 
+/**
+ * @brief Initializes a PPM image with specified width and height.
+ * @param im Pointer to the ppm_image structure to initialize.
+ * @param w Width of the image.
+ * @param h Height of the image.
+ * @return 0 on success, -1 on failure.
+ */
 int ppm_image_init(struct ppm_image *im, int w, int h);
+
+/**
+ * @brief Releases memory allocated for a PPM image.
+ * @param im Pointer to the ppm_image structure to release.
+ * @return 0 on success, -1 on failure.
+ */
 int ppm_image_release(struct ppm_image *im);
 
+/**
+ * @brief Sets the RGB values of a pixel in a PPM image.
+ * @param im Pointer to the ppm_image structure.
+ * @param x X-coordinate of the pixel.
+ * @param y Y-coordinate of the pixel.
+ * @param r Red channel value.
+ * @param g Green channel value.
+ * @param b Blue channel value.
+ */
 static inline void ppm_image_setpixel(struct ppm_image *im, int x, int y,
                                       unsigned char r, unsigned char g,
                                       unsigned char b) {
@@ -30,6 +72,12 @@ static inline void ppm_image_setpixel(struct ppm_image *im, int x, int y,
   ppm_setpixel(px, r, g, b);
 }
 
+/**
+ * @brief Writes the contents of a PPM image to a file.
+ * @param im Pointer to the ppm_image structure.
+ * @param path Path to the file where the image will be written.
+ * @return 0 on success, -1 on failure.
+ */
 int ppm_image_dump(struct ppm_image *im, char *path);
 
 #endif /* PPM_H */
